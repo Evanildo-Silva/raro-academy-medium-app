1d88711b9888eccbde31a2e5508912778fee42aa
Evanildo-Silva <evanildo.sf91@gmail.com>
2022-03-31 18:06:53 -0300

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,31 @@
-import React from 'react';
 import './App.css';
-/*App.tsx*/
-import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { LoginPage } from './pages/Login';
-import { NotFoundPage } from './pages/NotFound';
 import { ArtigosPage } from './pages/Artigos';
 import { ArtigoPage } from './pages/Artigo';
 import { MeusArtigosPage } from './pages/MeusArtigos';
 import { EditarArquivoPage } from './pages/EditarArquivo';
+import { NotFoundPage } from './pages/NotFound';
+import { Layout } from './components/Layout';
 
 
 function App() {
   return (
     <BrowserRouter>
-  <Routes>
-    <Route path="/login" element={<LoginPage />} />
-    <Route path="/" element={<ArtigosPage />} />
-    <Route path="/artigo/:id" element={<ArtigoPage />} />
-    <Route path="/artigos" element={<MeusArtigosPage />} />
-    <Route path="/artigos/editar/:id" element={<EditarArquivoPage />} />
-    <Route path="*" element={<NotFoundPage />} />
-  </Routes>
-</BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+
+        <Route path="/" element={<Layout />}>
+          <Route index element={<ArtigosPage />} />
+          <Route path="/artigo/:id" element={<ArtigoPage />} />
+          <Route path="/artigos" element={<MeusArtigosPage />} />
+          <Route path="/artigo/editar/:id" element={<EditarArquivoPage />} />
+          <Route path="/artigos/novo" element={<EditarArquivoPage />} />
+        </Route>
+
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
